2fdbfe0c54e3fcece2b610417a13fe1e83bc069c
mrendy58 <derendyyy58@gmail.com>
2023-04-10 10:43:35 +0700

    method

--- class_oop_122/class_oop_122.cpp
+++ class_oop_122/class_oop_122.cpp
@@ -5,8 +5,15 @@ class Mahasiswa {
 public:
     string nim;
     string nama; 
+public: void cetak() {
+    cout << "NIM =" << nim;
+    cout << "\nNama =" << nama;
+    }
 };
 
+class Matakuliah {
+
+};
 int main()
 {
     std::cout << "Hello World!\n";
